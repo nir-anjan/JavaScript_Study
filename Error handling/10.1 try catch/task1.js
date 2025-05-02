61de9d7c4753cb8d91338968ed0e2aad3a190e47
nir-anjan <tve21cs101@cet.ac.in>
2025-05-02 12:03:03 +0530

    try catch error handling

---
+++ Error handling/10.1 try catch/task1.js
@@ -0,0 +1,17 @@
+try {
+  // work work
+} catch (err) {
+  // handle errors
+} finally {
+  // cleanup the working space
+}
+
+//
+
+try {
+  // work work
+} catch (err) {
+  // handle errors
+}
+
+// cleanup the working space
